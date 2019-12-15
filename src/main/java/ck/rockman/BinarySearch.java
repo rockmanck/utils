107d6794bfcd513392cbd3015f30cbfb7d2adbb8
rockmanck <rockman.roll@gmail.com>
2019-12-15 19:42:47 +0200

    Updated gradle version. Implemented binary search with recursion.

--- src/main/java/ck/rockman/BinarySearch.java
+++ src/main/java/ck/rockman/BinarySearch.java
@@ -4,6 +4,11 @@ import java.util.Arrays;
 import java.util.List;
 
 public class BinarySearch {
+
+    private BinarySearch() {
+        // hide public constructor
+    }
+
     /**
      * {@code items} should be sorted in ascending order
      */
@@ -38,5 +43,27 @@ public class BinarySearch {
         } while (lo <= hi);
 
         return new BinarySearchResult<>(null, result);
+    }
+
+    public static <T extends Comparable<T>> BinarySearchResult<T> searchIndexWithRecursion(T searchTerm, List<T> items) {
+        int lo = 0;
+        int hi = items.size() - 1;
+        return recBinarySearch(items, searchTerm, lo, hi, 0);
+    }
+
+    private static <T extends Comparable<T>> BinarySearchResult<T> recBinarySearch(List<T> items, T searchTerm, int lo, int hi, int targetIndex) {
+        if (lo > hi) {
+            return new BinarySearchResult<>(null, targetIndex);
+        }
+        int mid = (lo + hi) / 2;
+        final T item = items.get(mid);
+        final int compareResult = item.compareTo(searchTerm);
+        if (compareResult == 0) {
+            return new BinarySearchResult<>(item, mid);
+        } else if (compareResult > 0) {
+            return recBinarySearch(items, searchTerm, lo, mid - 1, mid);
+        } else {
+            return recBinarySearch(items, searchTerm, mid + 1, hi, mid + 1);
+        }
     }
 }
--- src/test/java/ck/rockman/BinarySearchTest.java
+++ src/test/java/ck/rockman/BinarySearchTest.java
@@ -2,10 +2,13 @@ package ck.rockman;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class BinarySearchTest {
     @Test
@@ -26,8 +29,26 @@ public class BinarySearchTest {
         checkBinarySearch(193, list(0, 1, 2, 6, 7, 8, 9, 23, 76, 192, 465), null, 10);
     }
 
+    @Test
+    public void testBinarySearchPerformance() {
+        final int listSize = 10_000_000;
+        final List<Integer> list = new ArrayList<>(listSize);
+        final Random r = new Random(159);
+        for (int i = 0; i < listSize; i += 1) {
+            list.add(r.nextInt(50_000));
+        }
+        list.sort(Integer::compareTo);
+        final long t = System.nanoTime();
+        final BinarySearchResult<Integer> result = BinarySearch.searchIndexWithRecursion(159, list);
+        final double oneSecondInMilliseconds = 1_000;
+        final double time = (System.nanoTime() - t) / 1_000_000.0;
+        System.out.println("found index = " + result.getIndex() + " found item = " + result.getMatchedItem());
+        System.out.println("search time = " + time + "ms");
+        assertTrue("Search time more than one second", time < oneSecondInMilliseconds);
+    }
+
     private void checkBinarySearch(Integer searchTerm, List<Integer> list, Integer expectedItem, int expectedIndex) {
-        assertEquals(new BinarySearchResult<>(expectedItem, expectedIndex), BinarySearch.searchIndex(searchTerm, list));
+        assertEquals(new BinarySearchResult<>(expectedItem, expectedIndex), BinarySearch.searchIndexWithRecursion(searchTerm, list));
     }
 
     private List<Integer> list(Integer... values) {
